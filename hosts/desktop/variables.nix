8c54ad13f469db1a928ee056f47957876b4c5f06
Ametic <delciakx@gmail.com>
2025-06-24 17:50:39 -0500

    First commit

---
+++ hosts/desktop/variables.nix
@@ -0,0 +1,58 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Ametic";
+  gitEmail = "delciakx@gmail.com";
+
+  # Hyprland Settings
+  # ex "monitor=HDMI-A-1, 1920x1080@60,auto,1"
+  #
+  extraMonitorSettings = "
+    monitor = DP-2, 1920x1080@180, 0x0, 1 
+    monitor = HDMI-A-1, 1920x1080@72, 1920x0, 1
+    ";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "floorp"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "ghostty"; # Set Default System Terminal
+  keyboardLayout = "pl";
+  consoleKeyMap = "pl";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+
+  # Enable Printing Support
+  printEnable = true;
+
+  # Set Stylix Image
+  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
+
+  # Set Waybar
+  # Includes alternates such as:
+  # Just uncomment the one you want and comment out the others
+
+  #waybarChoice = ../../modules/home/waybar/Jerry-waybars.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-nekodyke.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
+  waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
+
+  # Set Animation style
+  # Available options are:
+  # animations-def.nix  (standard)
+  # animations-end4.nix (end-4 project)
+  # animations-dynamic.nix (ml4w project)
+  # animations-moving.nix (ml4w project)
+  # Just change the name after the - and rebuild
+  animChoice = ../../modules/home/hyprland/animations-end4.nix;
+
+  # Enable Thunar GUI File Manager
+  thunarEnable = true;
+}
