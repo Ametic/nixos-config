932d2e40982c39a731f8ded7792caaa5d4dd07a3
Ametic <delciakx@gmail.com>
2025-06-30 19:20:17 +0200

    Add install-ameos.sh script and rewrite fastfetch

--- modules/home/fastfetch/default.nix
+++ modules/home/fastfetch/default.nix
@@ -17,113 +17,84 @@
         height = 10;
         width = 20;
         padding = {
-          top = 2;
+          top = 7;
           left = 2;
         };
       };
 
       modules = [
         "break"
+        "break"
+        "break"
+        {
+          type = "custom";
+          format = " {#90}   {#31}   {#32}   {#33}   {#34}   {#35}   {#36}   {#37}  ";
+        }
+        "break"
+        {
+          type = "title";
+          keyWidth = 10;
+        }
+        "break"
         {
           type = "os";
-          key = "OS - ZaneyOS v2.3.2";
-          keyColor = "31";
+          key = " ";
+          keyColor = "34";
         }
         {
           type = "kernel";
-          key = " ├  ";
-          keyColor = "31";
+          key = " ";
+          keyColor = "34";
         }
         {
           type = "packages";
-          key = " ├ 󰏖 ";
-          keyColor = "31";
+          key = " ";
+          keyColor = "34";
         }
         {
           type = "shell";
-          key = " └  ";
-          keyColor = "31";
+          key = " ";
+          keyColor = "34";
         }
         "break"
         {
           type = "wm";
-          key = "WM   ";
-          keyColor = "32";
+          key = " ";
+          keyColor = "34";
         }
         {
-          type = "wmtheme";
-          key = " ├ 󰉼 ";
-          keyColor = "32";
-        }
-        {
-          type = "icons";
-          key = " ├ 󰀻 ";
-          keyColor = "32";
-        }
-        {
-          type = "cursor";
-          key = " ├  ";
-          keyColor = "32";
-        }
-        {
-          type = "terminal";
-          key = " ├  ";
-          keyColor = "32";
-        }
-        {
-          type = "terminalfont";
-          key = " └  ";
-          keyColor = "32";
-        }
-        "break"
-        {
-          type = "host";
-          format = "{5} {1} Type {2}";
-          key = "PC   ";
-          keyColor = "33";
-        }
-        {
-          type = "cpu";
-          format = "{1} ({3}) @ {7} GHz";
-          key = " ├  ";
-          keyColor = "33";
-        }
-        {
-          type = "gpu";
-          format = "{1} {2} @ {12} GHz";
-          key = " ├ 󰢮 ";
-          keyColor = "33";
-        }
-        {
-          type = "memory";
-          key = " ├  ";
-          keyColor = "33";
-        }
-        {
-          type = "disk";
-          key = " ├ 󰋊 ";
-          keyColor = "33";
-        }
-        {
-          type = "monitor";
-          key = " ├  ";
-          keyColor = "33";
-        }
-        {
-          type = "player";
-          key = " ├ 󰥠 ";
-          keyColor = "33";
+          type = "uptime";
+          key = " ";
+          keyColor = "34";
         }
         {
           type = "media";
-          key = " └ 󰝚 ";
-          keyColor = "33";
+          key = "󰝚 ";
+          keyColor = "34";
         }
         "break"
         {
-          type = "uptime";
-          key = "   Uptime   ";
+          type = "cpu";
+          key = " ";
+          keyColor = "blue";
         }
+        {
+          type = "gpu";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "memory";
+          key = " ";
+          keyColor = "blue";
+        }
+        "break"
+        {
+          type = "custom";
+          format = " {#90}   {#31}   {#32}   {#33}   {#34}   {#35}   {#36}   {#37}  ";
+        }
+        "break"
+        "break"
       ];
     };
   };
